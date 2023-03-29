d68a905b77b1de905f7edc0488a7aa4ca36d0e09
fspjonny <fabiosilvapedro@yahoo.com.br>
2023-03-29 16:01:43 -0300

    file updated

--- src/components/DisplayGrid.js
+++ src/components/DisplayGrid.js
@@ -1,6 +1,6 @@
 export default function DisplayGrid(props) {
     return (
-        <div className="flex flex-col items-center mt-4
+        <div className="flex flex-col items-center mt-[350px] md:mt-[260px]
         md:grid md:gap-4 md:grid-cols-4 md:auto-rows-auto md:p-4">
             {props.children}
         </div>
